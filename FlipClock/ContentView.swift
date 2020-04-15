8157afb21741b225083669d685c7be543aaf3777
linshaolong5240 <634205468@qq.com>
2020-04-16 00:15:06 +0800

    Initial Commit

---
+++ FlipClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlipClock
+//
+//  Created by 林少龙 on 2020/4/16.
+//  Copyright © 2020 teeloong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
